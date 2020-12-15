453d98a5a9a70d2bcf4b2c85248d40db5182e2e6
KirkinD <dkirkin@bostonsd.ru>
2020-12-15 12:08:01 +0300

    Refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,25 +14,28 @@ class HomeController < ApplicationController
   #Initializing search process and creating db entrance
   def create
     @search = Search.new home_params
-    if @search.vk_link == "" and @search.twi_link == "" and @search.inst_link == ""
+    if @search.invalid?
       return not_found
     end
-    @vk = VkontakteApi::Client.new '5c4469995c4469995c446999b75c314cc355c445c44699903fac963479419a58644cb51'
-    user = @vk.users.get(user_ids: @search.vk_link, fields:'photo_100', v:5.126)
-    @search.vk_first_name = user.first.first_name
-    @search.vk_last_name = user.first.last_name
-    @search.vk_photo_link = user.first.photo_100
-
+    #Insert VK credentials
+    vk_search = @search.vk_link.nil? ? @search.twi_link : @search.vk_link
+    vk_user = @search.get_vk vk_search
+    if !vk_user.nil?
+      @search.vk_first_name = vk_user.first.first_name
+      @search.vk_last_name = vk_user.first.last_name
+      @search.vk_photo_link = vk_user.first.photo_100
+    end
+    #Insert Twitter credentials
     twi_search = @search.twi_link.nil? ? @search.vk_link : @search.twi_link
-    twi_user = parse_twitter(twi_search)
+    twi_user = @search.get_twitter(twi_search)
     if !twi_user.nil?
       @search.twi_full_name = twi_user.full_name
       @search.twi_user_id = twi_user.user_id
       @search.twi_photo_link = twi_user.photo_link
     end
-
+    #insert Instagram credentials
     inst_search = @search.inst_link.nil? ? @search.vk_link : @search.inst_link
-    inst_user = get_inst inst_search
+    inst_user = @search.get_inst inst_search
     if !inst_user.nil?
       @search.inst_fullname = inst_user.full_name
       @search.inst_photo_link = inst_user.photo_link
@@ -47,52 +50,4 @@ class HomeController < ApplicationController
     params.require(:search).permit(:vk_link, :inst_link, :twi_link)
   end
 
-  #parsing twitter credentials
-  private def parse_twitter(username)
-    url = URI("https://peerreach-peerreach-subscription.p.rapidapi.com/user/lookup.json?screen_name=#{username}")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(url)
-    request["x-rapidapi-key"] = '6a8006dfc8msh9d6af52ac22eaf1p1350dcjsn07c12c474009'
-    request["x-rapidapi-host"] = 'peerreach-peerreach-subscription.p.rapidapi.com'
-
-    response = http.request(request)
-    hash = JSON.parse response.read_body
-    if hash.empty?
-      return nil
-    end
-    full_name = username
-    user_id = hash['user_id']
-    photo_link = "https://twitter.com/#{username}/photo"
-    result = Credentials.new(full_name, user_id, photo_link )
-    return result
-  end
-
-  private def get_inst(username)
-    url = URI("https://instagram30.p.rapidapi.com/rapi/user/#{username}?username=#{username}")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(url)
-    request["x-rapidapi-key"] = '6a8006dfc8msh9d6af52ac22eaf1p1350dcjsn07c12c474009'
-    request["x-rapidapi-host"] = 'instagram30.p.rapidapi.com'
-
-    response = http.request(request)
-    hash = JSON.parse response.read_body
-    if hash.empty?
-      return nil
-    end
-    puts hash
-    full_name = hash["fullname"]
-    photo_link = hash["profilePicUrl"]
-    user_id = hash["id"]
-    puts full_name, user_id
-    result = Credentials.new(full_name, user_id, photo_link)
-    return result
-  end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,63 @@
 class Search < ActiveRecord::Base
+
+  #checks params
+  def invalid?
+    vk_link == "" and twi_link == "" and inst_link == ""
+  end
+
+  #getting vk_info
+  def get_vk(user_id)
+    @vk = VkontakteApi::Client.new '5c4469995c4469995c446999b75c314cc355c445c44699903fac963479419a58644cb51'
+    user = @vk.users.get(user_ids: user_id, fields:'photo_100', v:5.126)
+  end
+
+  #getting inst info
+  def get_inst(username)
+    url = URI("https://instagram30.p.rapidapi.com/rapi/user/#{username}?username=#{username}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = '6a8006dfc8msh9d6af52ac22eaf1p1350dcjsn07c12c474009'
+    request["x-rapidapi-host"] = 'instagram30.p.rapidapi.com'
+
+    response = http.request(request)
+    hash = JSON.parse response.read_body
+    if hash.empty?
+      return nil
+    end
+    puts hash
+    full_name = hash["fullname"]
+    photo_link = hash["profilePicUrl"]
+    user_id = hash["id"]
+    puts full_name, user_id
+    result = Credentials.new(full_name, user_id, photo_link)
+    return result
+  end
+
+  #getting twitter credentials
+  def get_twitter(username)
+    url = URI("https://peerreach-peerreach-subscription.p.rapidapi.com/user/lookup.json?screen_name=#{username}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = '6a8006dfc8msh9d6af52ac22eaf1p1350dcjsn07c12c474009'
+    request["x-rapidapi-host"] = 'peerreach-peerreach-subscription.p.rapidapi.com'
+
+    response = http.request(request)
+    hash = JSON.parse response.read_body
+    if hash.empty?
+      return nil
+    end
+    full_name = username
+    user_id = hash['user_id']
+    photo_link = "https://twitter.com/#{username}/photo"
+    result = Credentials.new(full_name, user_id, photo_link )
+    return result
+  end
 end
